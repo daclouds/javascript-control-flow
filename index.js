7424ccec26c50558c426015921073b3521dbb1b6
daclouds <daclouds@gmail.com>
2014-11-28 22:55:16 +0900

    for-loop

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 var name = "daclouds";
 console.log("Hello, " + name);
 
+function elementHighlighter(elements, color) {
+	for (var i=0; i < elements.length; i++) {
+		elements[i].style['background-color'] = color;
+	}
+	
+}
+
 var blockElements = document.getElementById("blockElements");
 var inlineElements = document.getElementById("inlineElements");
+
+blockElements.addEventListener("click", function(){
+    var h1 = document.getElementsByTagName("h1");
+	elementHighlighter(h1, '#259b24');
+
+	var div = document.getElementsByTagName("div");
+	elementHighlighter(div, '#aed581');
+});
+
+inlineElements.addEventListener("click", function(){
+	var b = document.getElementsByTagName("b");
+
+	elementHighlighter(b, '#8bc34a');
+});
