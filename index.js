fc4253ae1d5e88cacd2205e351c56f477db2dd64
daclouds <daclouds@gmail.com>
2014-11-28 22:58:54 +0900

    switch, case

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 var name = "daclouds";
 console.log("Hello, " + name);
 
-function elementHighlighter(elements, color) {
+function elementHighlighter(elements, tagName) {
 	for (var i=0; i < elements.length; i++) {
-		elements[i].style['background-color'] = color;
+		switch (tagName) {
+			case 'h1':
+				elements[i].style['background-color'] = '#259b24';
+				break;
+			case 'div':
+				elements[i].style['background-color'] = '#aed581';
+				break;
+			case 'b':
+				elements[i].style['background-color'] = '#8bc34a';
+				break;
+			default:
+				break;
+		}
+		
 	}
 	
 }
@@ -13,14 +26,14 @@ var inlineElements = document.getElementById("inlineElements");
 
 blockElements.addEventListener("click", function(){
     var h1 = document.getElementsByTagName("h1");
-	elementHighlighter(h1, '#259b24');
+	elementHighlighter(h1, 'h1');
 
 	var div = document.getElementsByTagName("div");
-	elementHighlighter(div, '#aed581');
+	elementHighlighter(div, 'div');
 });
 
 inlineElements.addEventListener("click", function(){
 	var b = document.getElementsByTagName("b");
 
-	elementHighlighter(b, '#8bc34a');
+	elementHighlighter(b, 'b');
 });
