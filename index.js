27002ac1d872d5d78af06ceda77201b9073a0641
daclouds <daclouds@gmail.com>
2014-11-28 22:31:19 +0900

    block elements and inline elements

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 var name = "daclouds";
 console.log("Hello, " + name);
+
+var blockElements = document.getElementById("blockElements");
+var inlineElements = document.getElementById("inlineElements");
